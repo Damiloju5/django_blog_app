e2df766903ba6575c79158d113b9528cd7bcabf6
Damiloju5 <jesudamilojuafolabi@gmail.com>
2022-06-17 09:45:31 +0100

    blog post model added

--- I4G009761STJ/blog/models.py
+++ I4G009761STJ/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
